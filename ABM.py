711123ac2d4aeb46c1c915b385d0acb65a9a8d74
gonplaza <65181458+gonplaza@users.noreply.github.com>
2024-03-08 12:01:30 +0000

    Summarise all info at end of loop

--- ABM.py
+++ ABM.py
@@ -19,9 +19,11 @@ from operator import attrgetter
 from SSTD3 import Agent, ReplayBuffer, OUNoise, Critic, Actor, Softmax
 import torch
 
-number_of_simulations = 2
+number_of_simulations = 10
 Portfolio_size_data = []
 Simulation_number = []
+Statistics_on_VCs = []
+Portfolio_data = []
 
 for sim in range(number_of_simulations):
 
@@ -32,11 +34,11 @@ for sim in range(number_of_simulations):
 
     ## VC Coefficients
     # VC Coefficients - general
-    Number_of_VCs = 100 # 48,000 VCs in the world, but keeping it to 100 for computational reasons
+    Number_of_VCs = 10 # 48,000 VCs in the world, but keeping it to 100 for computational reasons
     Fund_maturity = 40 # number of time steps to realise returns (10 years) - each time step is 3 months (one quarter)
     Startup_exit = 32 # number of time steps it takes a startup to exit (8 years)
     Average_portfolio_size = 32 #Based on real world data
-    min_endowment_per_investment = 0.005 # Avoids the algorithm from making investments too small - limits max number of investments to 200 per fund.
+    min_endowment_per_investment = 0.005 # Avoids the algorithm from making investments too small - limits max number of investments to 200 per fund (with noise accounted for, which sum to max 0.005)
 
     # VC Coefficients - VC quality and advisory
     VC_quality_shape = 0.385 # shape coefficient for lognormal distribution
@@ -53,7 +55,7 @@ for sim in range(number_of_simulations):
     # List and corresponding probabilities of number of investment professionals (here called analysts) in a VC firm - used for calculating total working hours per time step
     Number_of_analysts_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
     Number_of_analysts_probabilities = [0.3042, 0.2195, 0.1531, 0.0966, 0.0649, 0.0430, 0.0282, 0.0223, 0.0156, 0.0126, 0.0083, 0.0066, 0.0041, 0.0038, 0.0029, 0.0024, 0.0019, 0.0017, 0.0012, 0.0011, 0.0012, 0.0012, 0.0010, 0.0012, 0.0011]
-    Hours_per_week_analyst = 50 # Number of hours worked by an analyst in a VC firm
+    Hours_per_week_analyst = 55 # Number of hours worked by an analyst in a VC firm
     Weeks_per_quarter = 13 # 52 weeks per year, so 13 weeks per quarter 
     Hours_per_quarter_analyst = Hours_per_week_analyst*Weeks_per_quarter # 1 time step = 3 months = 1 quarter
     Percentage_of_time_on_other_activities = 0.3 # Time spend by VC employee on activitties not related to either screening/DD or advising (e.g., fundraising)
@@ -91,6 +93,10 @@ for sim in range(number_of_simulations):
     # Startup Coefficeints - Sub_Industries - same probability for each - random choice
     List_of_Sub_Industries = ["Sub_Industry_1", "Sub_Industry_2", "Sub_Industry_3", "Sub_Industry_4", "Sub_Industry_5"]
     Probability_Distribution_of_Sub_Industries = [0.2, 0.2, 0.2, 0.2, 0.2]
+    # Matrix of correlations between subindustries
+    Subindustry_correlation_matrix = pd.read_excel("Sub_Industry_Correlation_Matrix.xlsx")
+    Subindustry_correlation_matrix.index = Subindustry_correlation_matrix["Unnamed: 0"]
+    del Subindustry_correlation_matrix["Unnamed: 0"]
 
     # Startup Coefficients - Due Diligence
     Noise_mean_before_DD = 0 # mean for normal distribution of noise before due diligence
@@ -136,7 +142,7 @@ for sim in range(number_of_simulations):
             self.Effort_available = self.Investment_analysts*Time_for_DD_and_advising_per_analyst_per_fund   # Total number of hours available for a fund per time step for DD and advising
             self.Effort_allocated_to_startups_in_portfolio = self.Portfolio_size*Advising_time # Effort allocated to advising startups currently in the portfolio
             self.Effort_left_for_DD = self.Effort_available - self.Effort_allocated_to_startups_in_portfolio # Effort left for DD after taking away the effort allocated to advisory
-            self.Number_of_available_screenings = self.Effort_left_for_DD/DD_time # Number of possible screenings per time step based on the effort left for DD
+            self.Number_of_available_screenings = int((self.Effort_left_for_DD/DD_time)+1) # Number of possible screenings per time step based on the effort left for DD (rounded up)
             
             self.Remaining_of_investment_stage = max(0, (Fund_maturity - Startup_exit - self.Fund_age)) # Investment stage is 2 years, so (10 years - 8 years - fund age)
         
@@ -182,10 +188,23 @@ for sim in range(number_of_simulations):
                     for j in Portfolio:
                         weight_i = i[1]/(1-self.Endowement)
                         weight_j = j[1]/(1-self.Endowement)
-                        sd_i = Noise_sd_before_DD[getattr(i[0], "Sub_Industry")]
-                        sd_j = Noise_sd_before_DD[getattr(j[0], "Sub_Industry")]
-                        Total_variance = (weight_i*weight_j*sd_i*sd_j) + Total_variance # Assuming a correlation coefficient equal to 1 (perfectly correlated) as same industry
+                        #########
+                        sd_i = (Noise_sd_before_DD[getattr(i[0], "Sub_Industry")]**2 + Idiosyncratic_risk_sd**2)**(1/2)
+                        sd_j = (Noise_sd_before_DD[getattr(j[0], "Sub_Industry")]**2 + Idiosyncratic_risk_sd**2)**(1/2)
+                        Correlation_coeff =  float(Subindustry_correlation_matrix.loc[getattr(i[0], "Sub_Industry"), getattr(j[0], "Sub_Industry")]) 
+                        Total_variance = (weight_i*weight_j*Correlation_coeff*sd_i*sd_j) + Total_variance
             return Total_variance
+        
+        def portfolio_variance(self, Portfolio):
+            Total_variance = 0
+            Avg_return = 0
+            for i in Portfolio:
+                Avg_return = float(self.Growth_to_returns(getattr(i[0], "Growth"))) + Avg_return
+            Avg_return = Avg_return/self.Portfolio_size
+            for j in Portfolio:
+                Total_variance = abs((float(self.Growth_to_returns(getattr(i[0], "Growth"))) - Avg_return)**(2)) + Total_variance
+            return Total_variance
+
         
             
         # Expected Sharpe ratio
@@ -194,6 +213,12 @@ for sim in range(number_of_simulations):
                 return 0
             else:
                 return float(self.expected_return(Portfolio) - Compounded_risk_free_rate)/float(self.expected_portfolio_variance(Portfolio))
+            
+        def final_Sharpe_ratio(self, Portfolio):
+            if len(Portfolio) == 0:
+                return 0
+            else:
+                return float(self.final_return(Portfolio) - Compounded_risk_free_rate)/float(self.portfolio_variance(Portfolio))
         
         """
         def expected_portfolio_downside_deviation(self, Portfolio):
@@ -218,8 +243,36 @@ for sim in range(number_of_simulations):
             else:
                 return float(self.expected_return(Portfolio) - Compounded_risk_free_rate)/float(self.expected_portfolio_downside_deviation(Portfolio))
         """
-        
+        """
         # Gets reward after taking action a 
+        def get_reward(self, action, old_portfolio):
+            # Only can invest if within investment period
+            if self.Fund_age <= (Fund_maturity - Startup_exit):  
+                # Endowment cannot be negative 
+                if action < 0:
+                    return torch.tensor([-100*(-action[0])])
+                # If less than the minimum endowment was invested, we assume that VC does not invest into a given startup
+                if 0 < action < min_endowment_per_investment:
+                    return torch.tensor([0])
+                # If action is more than the minimum endowment but less than 1, then VC invests in startup, and the reward is the Sortino ratio after minus Sortino ratio before
+                if min_endowment_per_investment <= action <= 1 and action <= self.Endowement:
+                    #return torch.tensor([(self.expected_Sortino_ratio((self.Portfolio + [list(startup) + list(action)])) - self.expected_Sortino_ratio(self.Portfolio))])
+                    return torch.tensor([(self.expected_Sharpe_ratio(self.Portfolio) - self.expected_Sharpe_ratio(old_portfolio))])
+                # If there is not enough endowment, no investment occurs
+                if min_endowment_per_investment <= action <= 1 and action > self.Endowement:
+                    return torch.tensor([-100*(action[0]-self.Endowement)])
+                # Action cannot be more than 1
+                if action>1:
+                    return torch.tensor([-100*action[0]])
+            # No investment if investment period is past
+            elif world.counter == (Fund_maturity-Startup_exit):
+                return torch.tensor([-self.Endowement])
+            elif world.counter == (Fund_maturity - 1):
+                return torch.tensor([self.final_Sharpe_ratio(self.Portfolio)])
+            else:
+                return torch.tensor([-10])
+        """    
+                # Gets reward after taking action a 
         def get_reward(self, action, old_portfolio):
             # Only can invest if within investment period
             if self.Fund_age <= (Fund_maturity - Startup_exit):  
@@ -249,9 +302,12 @@ for sim in range(number_of_simulations):
             ## Prospect attributes
             # Attribtue 1 - prospect growth as perceived by agent (VC)
             Prospect_Growth = getattr(Prospect, "Growth_after_DD")
-            # Attribute 2 - growth standard deviation based on subindustry
-            Sub_Industry = getattr(Prospect, "Sub_Industry")
-            Growth_sd = Noise_sd_before_DD[Sub_Industry]
+            # Attribute 2 - Average correlation of prospect with portfolio
+            Avg_corr = 0
+            for i in self.Portfolio:    
+                Avg_corr =  float(Subindustry_correlation_matrix.loc[getattr(Prospect, "Sub_Industry"), getattr(i[0], "Sub_Industry")]) + Avg_corr
+            if self.Portfolio_size != 0:
+                Avg_corr = Avg_corr/self.Portfolio_size
             
             ## Cohort attributes
             # Attibute 3 - average growth of prospects, as perceived by agent (VC)
@@ -291,7 +347,7 @@ for sim in range(number_of_simulations):
             # Attribute 10 - Remaining of investment stage as a percentage
             Remaining_of_investment_stage = self.Remaining_of_investment_stage/(Fund_maturity - Startup_exit)
             
-            state_ = torch.tensor([Prospect_Growth, Growth_sd, Screenings_mean, Screenings_sd, Portfolio_mean, Portfolio_sd, Percentage_screening_left, VC_quality, Endowement, Remaining_of_investment_stage])
+            state_ = torch.tensor([Prospect_Growth, Avg_corr, Screenings_mean, Screenings_sd, Portfolio_mean, Portfolio_sd, Percentage_screening_left, VC_quality, Endowement, Remaining_of_investment_stage])
             return state_
         
         # Gets next state 
@@ -299,8 +355,8 @@ for sim in range(number_of_simulations):
             ## Prospect attributes - no prospects on next state, so prospect attributes are null
             # Attribute 1 - prospect growth as perceived by agent (VC)
             Prospect_growth = 0
-            # Attribute 2 - growth standard deviation based on subindustry
-            Growth_sd = 0
+            # Attribute 2 - Average correlation of prospect with portfolio
+            Avg_corr = 0
             
             ## Cohort attributes
             # Attribute 3 - average growth of prospects, as perceived by agent (VC)
@@ -340,7 +396,7 @@ for sim in range(number_of_simulations):
             # Attribute 10 - Remaining of investment stage as a percentage
             Remaining_of_investment_stage = self.Remaining_of_investment_stage/(Fund_maturity - Startup_exit)
             
-            next_state_ = torch.tensor([Prospect_growth, Growth_sd, Screenings_mean, Screenings_sd, Portfolio_mean, Portfolio_sd, Percentage_screening_left, VC_quality, Endowement, Remaining_of_investment_stage])
+            next_state_ = torch.tensor([Prospect_growth, Avg_corr, Screenings_mean, Screenings_sd, Portfolio_mean, Portfolio_sd, Percentage_screening_left, VC_quality, Endowement, Remaining_of_investment_stage])
             return next_state_
         
         # Executes the changes that occur at each time step
@@ -367,7 +423,7 @@ for sim in range(number_of_simulations):
 
             self.Effort_allocated_to_startups_in_portfolio = self.Portfolio_size*Advising_time
             self.Effort_left_for_DD = self.Effort_available - self.Effort_allocated_to_startups_in_portfolio
-            self.Number_of_available_screenings = self.Effort_left_for_DD/DD_time 
+            self.Number_of_available_screenings = int((self.Effort_left_for_DD/DD_time)+1) # round up
 
             self.Remaining_of_investment_stage = max(0, (Fund_maturity - Startup_exit - self.Fund_age)) # update the number of time steps left for the end of investment stage
             agent.save_model_parameters()
@@ -407,7 +463,7 @@ for sim in range(number_of_simulations):
                 advisory_factor = self.average_investor_quality()-advisory_VC_quality_delta # VC quality distribution but same mean as the revenue growoth with good advisory
                 self.Growth = Alpha*self.Growth + (Beta*advisory_factor - additional_return_with_advisory_per_time_step) + np.random.normal(Idiosyncratic_risk_mean, Idiosyncratic_risk_sd)
             self.Life_stage += 1    
-            # Normalise so that there is no revenue growth of below -100% (impossible) or above 100% (too extreme - 256 multiple)
+            # Normalise so that there is no revenue growth of below -95% (impossible) or above 95% (cdf = 0.9988207293344418)
             if self.Growth < -1:
                 self.Growth = -1
             if self.Growth > 1:
@@ -435,9 +491,9 @@ for sim in range(number_of_simulations):
                 VC_quality_cdf = skewnorm.cdf(self.Growth_with_noise, Growth_a, Growth_loc, Growth_scale)
                 noise_reduction_VC_quality = lognorm.ppf(VC_quality_cdf, VC_quality_shape, VC_quality_loc, VC_quality_scale)/VC_quality_max
                 # Ensure that the normalised VC quality for noise reduction is between 0 and 1
-                if noise_reduction_VC_quality <= 0:
+                if noise_reduction_VC_quality <= 0.01:
                     noise_reduction_VC_quality = 0.01
-                if noise_reduction_VC_quality >= 1:
+                if noise_reduction_VC_quality >= 0.99:
                     noise_reduction_VC_quality = 0.99
             # if there are VC investors, the average of the two means is taken
             else:
@@ -501,9 +557,9 @@ for sim in range(number_of_simulations):
                 # for VC quality, draw a TVPI based on the TVPI distribution for VC quality and normalise
                 Normalised_TVPI = lognorm.rvs(VC_quality_shape, VC_quality_loc, VC_quality_scale)/VC_quality_max
                 # Ensure that the normalised VC quality for noise reduction is between 0 and 1:
-                if Normalised_TVPI <= 0:
+                if Normalised_TVPI <= 0.01:
                     Normalised_TVPI = 0.01
-                if Normalised_TVPI >= 1:
+                if Normalised_TVPI >= 0.99:
                     Normalised_TVPI = 0.99
                 # Normalised_TVPI is passed as VC quality
                 a = VC(i,float(Normalised_TVPI),random.choices(Number_of_analysts_list, Number_of_analysts_probabilities)[0],0,self)
@@ -573,22 +629,16 @@ for sim in range(number_of_simulations):
         world.step()
 
     # Get statistics on VC agents
-    Statistics_on_VCs = []
     for i in world.VCs:
-        Statistics_on_VCs = [[i.unique_id, i.VC_quality, i.Portfolio_size, i.Endowement, i.Investment_analysts, i.final_return(i.Portfolio)]] + Statistics_on_VCs
+        Statistics_on_VCs = [[sim, i.unique_id, i.VC_quality, i.Portfolio_size, i.Endowement, i.Investment_analysts, i.final_return(i.Portfolio)]] + Statistics_on_VCs
 
-    # Export the statistics on VC agents to an Excel spreadsheet
-    df = pd.DataFrame(np.array(Statistics_on_VCs), columns = ["Unique_id", "VC_Quality","Portfolio_size", "Endowement", "Investment_analysts", "Final_return"])
-    df.to_excel("Statistics_on_VCs.xlsx")
-    #print(df)
 
-    avg_portfolio_size = 0
-    Portfolio_data = []    
+    avg_portfolio_size = 0    
     for i in world.VCs:
         avg_portfolio_size = getattr(i, "Portfolio_size") + avg_portfolio_size
         for j in i.Portfolio:
             # Transit used to record all the data in a spreadsheet
-            Transit = [i.unique_id, j[0].unique_id, j[0].Growth, i.Growth_to_returns(j[0].Growth)]
+            Transit = [sim, i.unique_id, j[0].unique_id, j[0].Growth, i.Growth_to_returns(j[0].Growth)]
             # Access elements from j[1] onwards - do not include j[0] as this is the startup itself (j[1] onwards includes amount invested, growth after DD, and fund age)
             for z in j[1:]:
                 Transit.append(z)
@@ -600,10 +650,14 @@ for sim in range(number_of_simulations):
     Portfolio_size_data.append(avg_portfolio_size)
     Simulation_number.append(sim)
 
-    # Export the portfolio data to a spreadsheet
-    df2 = pd.DataFrame(np.array(Portfolio_data), columns = ["Unique_id_VC", "Unique_id_Startup", "Growth_final", "Return","Amount_Invested", "Growth_after_DD", "Fund_age_when_invested", "Sub_Industry"])
-    df2.to_excel("Portfolio_data.xlsx")
-    #print(df2) 
+
+# Export the statistics on VC agents to an Excel spreadsheet
+df = pd.DataFrame(np.array(Statistics_on_VCs), columns = ["Simulation_number","Unique_id", "VC_Quality","Portfolio_size", "Endowement", "Investment_analysts", "Final_return"])
+df.to_excel("Statistics_on_VCs.xlsx")
+
+# Export the portfolio data to a spreadsheet
+df2 = pd.DataFrame(np.array(Portfolio_data), columns = ["Simulation_Number","Unique_id_VC", "Unique_id_Startup", "Growth_final", "Return","Amount_Invested", "Growth_after_DD", "Fund_age_when_invested", "Sub_Industry"])
+df2.to_excel("Portfolio_data.xlsx")
 
 df3 = pd.DataFrame({"Simulation_Number": Simulation_number, "Average_Portfolio_Size": Portfolio_size_data})
 df3.to_excel("Portfolio_size_data.xlsx", index=False)
